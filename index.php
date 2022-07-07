bcb98d6c456e8718ae7ee3a8e2b8daac288a88b0
Christine Siebels <christine@mygeekola.com>
2022-07-07 17:58:54 -0600

    Creating landing page for presentation.

---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Clark Hulings Foundation Presentation</title>
+        <link rel="preload" as="image" href="slide1.png">
+        <link rel="preload" as="image" href="slide2.png">
+        <link rel="preload" as="image" href="slide3.png">                
+        <link rel="preload" as="image" href="slide4.png">        
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="images/favicon.ico" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+        <style type="text/css">
+            img {
+                border-radius: 10px;
+                padding: 3px 0;
+            }
+           
+        </style>
+    </head>
+    <body  class="bg-dark">
+        <!-- Responsive navbar-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <a class="navbar-brand" href="#!">Clark Hulings Foundataion</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="https://www.clarkhulings.com" target="_blank">About</a></li>
+                        <li class="nav-item"><a class="nav-link" href="https://www.clarkhulingsfoundation.org" target="_blank">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+         <!-- Content section-->
+         <section class="py-1">
+        <div class="container">
+          <div class="row">
+            <div class="col">
+                <h1 class="text-white fs-3 fw-bolder text-center">Clark Hurlings Art Work</h1>
+                <p class="text-white-50 mb-0 text-center">Click to Experience</p>
+            </div>
+        </div>
+          <div class="row">
+            <div class="col-sm">
+              <a href="../chf4/woodbearers.html" target="_blank"><img src="images/bg-woodbearers-footer.png" width="100%" class="img-fluid" /></a>
+            </div>
+            <div class="col-sm">
+             <a href="../chf4/pinkparasol.html" target="_blank"><img src="images/pinkparasol.png" width="100%" class="img-fluid" /></a>
+            </div>
+        
+            <div class="col-sm">
+            <a href="../chf4/soyalonga.html" target="_blank"><img src="images/soyalonga.png" width="100%" class="img-fluid" /></a>
+            </div>
+            <div class="col-sm">
+            <a href="../chf4/cart.html" target="_blank"><img src="images/cart-footer.png" width="100%" class="img-fluid" /></a>
+            </div>
+          </div>
+        </div>
+        </section>
+        <!-- Content section-->
+        <section class="py-1">
+            <div class="container my-5">
+                <div class="row justify-content-center">
+                    <div class="col-lg-12">
+                        <img class="img-fluid" src="images/slide1.png" alt="Clark Hulings Foundation" />
+                        <img class="img-fluid" src="images/slide2.png" alt="Clark Hulings Foundation" />                        
+                        <img class="img-fluid" src="images/slide3.png" alt="Clark Hulings Foundation" />
+                        <img class="img-fluid" src="images/slide4.png" alt="Clark Hulings Foundation" />    
+                        <img class="img-fluid" src="images/slide5.png" alt="Clark Hulings Foundation" />    
+                        <a href="https://www.youtube.com/watch?v=fwMU5hU6kjk&t=6s" target="_blank"><img class="img-fluid" src="images/slide5b.png" alt="Clark Hulings Foundation" /> </a>
+                        <img class="img-fluid" src="images/slide8.png" alt="Clark Hulings Foundation" />       
+                        <img class="img-fluid" src="images/slide11.png" alt="Clark Hulings Foundation" />    
+                        <a href="https://www.clarkhulings.com" target="_blank"><img class="img-fluid" src="images/slide11a.png" alt="Clark Hulings Foundation" /></a>    
+                        <a href="https://www.clarkhulingsfoundation.org" target="_blank"><img class="img-fluid" src="images/slide12.png" alt="Clark Hulings Foundation" /> </a>   
+                        <a href="mailto:elizabeth@clarkhulingsfoundation.org"><img class="img-fluid" src="images/slide13.png" alt="Clark Hulings Foundation" /></a>
+                    </div>
+                </div>
+            </div>
+        </section>
+       
+        <!-- Footer-->
+        <footer class="py-5 bg-dark">
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; <?php echo date('Y');?> | The Clark Hulings Foundation</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
